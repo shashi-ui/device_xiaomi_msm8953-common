a91f4b474ac4e1e2ce27903d9daf9c9602f1b4f7
dib12 <61652361+dib12@users.noreply.github.com>
2024-09-28 09:04:54 +0530

    fixup

--- msm8953.mk
+++ msm8953.mk
@@ -88,6 +88,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.bluetooth.default \
+    audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default
 
